c613f26897188d806303386733ebbd4b4fbc4c5f
Jack-Christopher <64046377+Jack-Christopher@users.noreply.github.com>
2021-07-04 08:11:25 -0500

    Examples added

--- main.cpp
+++ main.cpp
@@ -13,17 +13,13 @@
 int main()
 {
     LL1 parser;
-    parser.setGrammar("sample.txt");
+    parser.setGrammar("sample.txt", "E");
     parser.tas.readFromText("TAS.txt", 6, 5); // Se llena el TAS de 6x5 desde un archivo
     parser.tas.print();
-    std::string tempText ;
-    std::cout<< "Ingresa la cadena a analizar:\t";
-    std::cin.ignore();
-    std::getline(std::cin, tempText);
-    if (parser.analyze(tempText))
-        std::cout<< "Cadena Aceptada\n";
-    else
-        std::cout<< "Cadena NO Aceptada\n";
+    std::cout<<"\n";
+    parser.verify("id + id");
+    parser.verify("id - id");
+    parser.verify("id +");
 
     return 0;
 }
